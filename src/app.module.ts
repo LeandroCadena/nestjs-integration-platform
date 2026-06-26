fe0e7abbe98859feac2dceaefbd506f0517e8808
unknown <cadenaleandro97@gmail.com>
2026-06-26 17:03:01 -0300

    chore: establish application architecture and core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AppConfigModule } from './config/app-config.module';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AppConfigModule, PrismaModule],
 })
 export class AppModule {}
